517127bec0ea3f05564413d3c751568817ab3b9e
Madelyn Payne <mpayne@mit.edu>
2016-06-23 17:53:36 -0400

    tokenize imported txt and make words lowercasein word_postag_list

--- importing_txt_files.py
+++ importing_txt_files.py
@@ -6,7 +6,7 @@ Created on Tue Jun 21 16:28:20 2016
 """
 from __future__ import division
 import nltk, re, pprint
-from nltk import word_tokenize
+from nltk import sent_tokenize, word_tokenize
 '''
 import nltk
 with(open('/Users/Madelyn/Desktop/UROP/Story Corpus/A Cub Can.txt', 'r') as in_file):
@@ -19,18 +19,42 @@ print sents
 #book_file = open('/Users/Madelyn/Desktop/UROP/Story Corpus/A Cub Can.txt')
 #print book_file.read()
 
-f = open('/Users/Madelyn/Desktop/UROP/Story Corpus/A Cub Can.txt')
+f = open('./StoryCorpus/A Cub Can.txt')
 raw = f.read()
 
 print raw
 
+
+sent_tokenize_list = sent_tokenize(raw)#breaks raw text into list of its sentences
+#print len(sent_tokenize_list)
+print sent_tokenize_list
+
+word_sent_token = []
+#breaks each sentence into a  list of its parts
+for sent in sent_tokenize_list:
+    #print word_tokenize(sent)
+    word_sent_token.append(word_tokenize(sent))
+print '\n'
+pos_tag_sent_list = []
+
+#creates list of tuples for each part of each sentence (word, pos_tag) 
+for sent in word_sent_token:
+    print nltk.pos_tag(sent)
+    pos_tag_sent_list.append(nltk.pos_tag(sent))
+print '\n'
+print pos_tag_sent_list
+
+for sent in pos_tag_sent_list:
+    
+
+'''
 tokens = word_tokenize(raw)
 print tokens
 words = [w.lower() for w in tokens]
 print words
 vocab = sorted(set(words))
 print vocab
-
+'''
 
 
 
--- word_search.py
+++ word_search.py
@@ -4,122 +4,19 @@ Created on Tue Jun 21 13:55:30 2016
 
 @author: Madelyn
 """
+word_categories_list = []
 
 
-
-'''
 for key in my_dict:
-    value = my_dict[key]
-    for word in value:
-        print word #accessing the individual words of each header
-'''
-
-'''
-#the process works but takes forever (2.0694129467 seconds)
-##import time
-##start_time = time.time()
-import pyexcel
-sheet_list = []
-
-from pyexcel.cookbook import split_a_book
-split_a_book('./resources/WordCategories.xlsx', 'output.xlsx')
-import glob
-outputfiles = glob.glob('*_output.xlsx')#splits book to be single sheets
-for file in sorted(outputfiles):
-    sheet_list.append(file)#adds each new sheet to a list
-##print("--- %s seconds ---" % (time.time() - start_time))
-    
-print sheet_list
-print sheet_list[0]
-if sheet_list[0] == 'All_output.xlsx':
-    sheet1 = pyexcel.get_sheet(file_name = sheet_list[0], name_columns_by_row=0)
-    sheet1_od = sheet1.to_dict()
-    sheet1_dict = dict(sheet1_od)
-if sheet_list[1] == 'Easy_output.xlsx':
-    sheet2 = pyexcel.get_sheet(file_name = sheet_list[1], name_columns_by_row=0)
-    sheet2_dict = dict(sheet2.to_dict())
-else:
-    print "You don't have the appropriate sheet names"
-    
-#print sheet1_dict
-#print sheet2_dict
-
-
-#sheet1_od = sheet.to_dict()
-#sheet1_dict = dict(sheet1_od)
-#print sheet1_dict
-#sheet2_od = dict(sheet.to_dict())
-#book[sheet_index][row, column]
-
-
-word_count_dict_All = {}
-word_count_dict_Easy = {}
-
-def make_word_count_dict(my_dict, word_count_dict):
-    '''
-#    Takes word count from each category and puts them into their own dictionary
-#    
-#    Inputs: the original dictionary and the resulting word_count dictionary
-#    Outputs: returns word_count dictionary and original dictionary
-'''
-    for key in my_dict:
-        value = my_dict[key]
-        word_count = int(value[0])
-        word_count_dict[key] = word_count
-    return word_count_dict
-
-
-#a = make_word_count_dict(my_dict, word_count_dict)
-#print a
-
-def clean_dict(my_dict):
-    '''
-#    Removes empty spaces ('') from a dictionary
-#    
-#    Inputs: dictionary
-#    Outputs: revised dictionary
-'''
-    for key in my_dict:
-        value = my_dict[key]
-        value.remove(value[0])#gets rid of first elm of each value list which is word_count
-        while '' in value:#gets rid of blanks in all value lists
-            value.remove('')
-    del my_dict['Total Words'] #gets rid of total words key
-        #for word in value:
-           #str(word) #does this do anything?
-    return my_dict
-
-#b = clean_dict(my_dict)
-#print b
-
-def extra_clean_dict(my_dict):
-    '''
-#    Removes the word count from the dictionary
-#    Is separated from first clean because some 1st values were not numbers
-'''
-    for key in my_dict.keys():
-        if key == '': #takes care of case where key is blank for some reason
-            del my_dict[key]
-    return my_dict
-
-sheet1_dict = extra_clean_dict(sheet1_dict)
-sheet1_word_count = make_word_count_dict(sheet1_dict, word_count_dict_All)
-print sheet1_word_count
-print "\n"
-sheet1_dict = clean_dict(sheet1_dict)
-print sheet1_dict
-
-print "\n"
-
-sheet2_dict = extra_clean_dict(sheet2_dict)
-sheet2_dict = clean_dict(sheet2_dict)
-print sheet2_dict
-'''
+    if key 
+    word_categories_list.append(key)
 
 my_dict = {'Nouns': [ u'program', u'hole', u'bit', u'loss', u'role'] \
 ,'Adjectives': [u'happy', u'nearby', u'successful', u'busy']}
 
-word_postag_list = [('love','NN'), ('happy','adj')]
+postag_sent_list = [[('It', 'PRP'), ('can', 'MD'), ('dip', 'VB'), ('.', '.')],\
+    [('With', 'IN'), ('help', 'NN'), (',', ','), ('a', 'DT'), ('cub', 'NN'), ('can', 'MD'), ('grow', 'VB'), ('up', 'RP'), ('.', '.')]]
+word_postag_list = [('Love','NN'), ('happy','adj')]
 
 '''
 for key in my_dict:
@@ -128,12 +25,22 @@ for key in my_dict:
         print word #accessing the individual words of each header
 '''
 count = 0
+temp_list = []
 
-pos_mapping_dict = {'Nouns': 'NN', 'Adjectives': 'adj'}
+#for subclasses in categories (ex: Nouns > Animals) label both as the corresponding tag \
+#   so the word is searched for through all classes that are nouns
+
+pos_mapping_dict = {'Nouns': 'NN', 'Adjectives': 'adj', 'Animals': 'NN'}
 #names the two indices of each tuple_pair in the list
+    #maybe make the values lists to allow {'Verbs' : [VB, VBD, VBG, VBN, VBP, VBZ]}
+    #look for built-in function that collects all 'Verb' pos_tags, all 'Noun' pos_tags etc. 
 for tuple_pair in word_postag_list:
     word = tuple_pair[0]
     pos_tag = tuple_pair[1]
+    #makes words lowercase
+    word = word.lower()
+    temp_list.append((word, pos_tag))
+    word_postag_list = temp_list
     #iterates through each key in pos_mapping_dict
     for mapping_key in pos_mapping_dict:
         value = pos_mapping_dict[mapping_key] #value is a value in the pos_mapping_dict
@@ -145,8 +52,19 @@ for tuple_pair in word_postag_list:
                 count += 1 #gives a value of 1 for each word found in my_dict
 
 print count
-                     
-            
+
+'''
+temp_list = []
+
+for tuple_pair in word_postag_list:
+    word = tuple_pair[0]
+    pos_tag = tuple_pair[1]
+    word = word.lower()
+    temp_list.append((word, pos_tag))
+    word_postag_list = temp_list
+    print word
+print word_postag_list
+'''     
 '''    
 for tuple_pair in book_word_tuple_list:
     word = tuple_pair[0]
