f5396ebec6a48f393b7b45a535c1580c154b69a2
Madelyn Payne <mpayne@mit.edu>
2016-06-27 17:17:50 -0400

    Open multiple files and tokenize them

--- importing_txt_files.py
+++ importing_txt_files.py
@@ -22,113 +22,74 @@ def get_wordnet_pos(treebank_tag):
         return wordnet.ADV
     else:
         return ''
-#from nltk.tag.simplify import simplify_wsj_tag
 '''
-import nltk
-with(open('/Users/Madelyn/Desktop/UROP/Story Corpus/A Cub Can.txt', 'r') as in_file):
-    text = in_file.read()
-    sents = nltk.sent_tokenize(txt)
+import glob
+for filename in glob.glob('/resources/StoryCorpus/*.txt'):
+    print filename
+    f = open(filename)
+    raw = f.read()
+'''   
+import glob
+
+list_of_files = glob.glob('./resources/StoryCorpus/*.txt')
+
+story_error = []
+for fileName in list_of_files:
+    f = open( fileName)
+    raw = f.read()
     
-print sents
-'''
-
-#book_file = open('/Users/Madelyn/Desktop/UROP/Story Corpus/A Cub Can.txt')
-#print book_file.read()
-
-f = open('./StoryCorpus/A Cub Can.txt')
-raw = f.read()
-
-print raw
-
-
-sent_tokenize_list = sent_tokenize(raw)#breaks raw text into list of its sentences
-#print len(sent_tokenize_list)
-print sent_tokenize_list
-
-word_sent_token = []
-#breaks each sentence into a  list of its parts
-for sent in sent_tokenize_list:
-    #print word_tokenize(sent)
-    word_sent_token.append(word_tokenize(sent))
-print '\n'
-pos_tag_sent_list = []
-
-#creates list of tuples for each part of each sentence (word, pos_tag) 
-for sent in word_sent_token:
-    print nltk.pos_tag(sent)
-    pos_tag_sent_list.append(nltk.pos_tag(sent))
-print '\n'
-print pos_tag_sent_list
-
-lem_ready_word_list = []
-
-for sent in pos_tag_sent_list:
-    for tuple_pair in sent:
-        word = tuple_pair[0]
-        pos_tag = tuple_pair[1]
-        #gives correct pos_tag
-        pos_tag = get_wordnet_pos(pos_tag)
-        #print pos_tag
-        #makes words lowercase
-        word = word.lower()
-        lem_ready_word_list.append((word, pos_tag))
-
+#f = open('./resources/StoryCorpus/.txt')
+#raw = f.read()
     
-print lem_ready_word_list
+    word_sent_token = []
+    pos_tag_sent_list = []
+    lem_ready_word_list = []
+    final_words = []
 
-
-final_words = []
-
-for tuple_pair in lem_ready_word_list:
-    word = tuple_pair[0]
-    pos_tag = tuple_pair[1]
-    if pos_tag == '':
-        lem_word = wordnet_lemmatizer.lemmatize(word)
-        final_words.append(lem_word)
-    else:
-        lem_word = wordnet_lemmatizer.lemmatize(word, pos=pos_tag)
-        final_words.append(lem_word)
-
-print final_words
-
-    
-tot_word_value = 0
-word_count = 0
-rejected_words = []  
- 
-for word in final_words:
-    for key in ezsheet_dict:
-        value = ezsheet_dict[key]
-        if word in value:
-            tot_word_value += 1
-        elif:
-            for key in allsheet_dict:
-                value = allsheet_dict[key]
-                if word in value:
-                    tot_word_value += 2
-                    continue #will this line prevent giving extra points if word is in two columns of 'WordCategories'?
-        else:
-            rejected_words.append(word)
-    print word_count
-print word_count
-
-'''
-
-lemmatized_word_list = []
-for sent in simplified_list_of_lists:
-    for word in sent:
-        pass
-        word = word.lemmatize()
-        lemmatized_word_list.append((word, pos_tag))
+    try: 
+        #tokenize
+        sent_tokenize_list = sent_tokenize(raw)#breaks raw text into list of its sentences
+        #breaks each sentence into a  list of its parts
+        for sent in sent_tokenize_list:
+            word_sent_token.append(word_tokenize(sent))
+        #creates list of tuples for each part of each sentence (word, pos_tag) 
+        for sent in word_sent_token:
+            pos_tag_sent_list.append(nltk.pos_tag(sent))
+        #pos_tag
+        for sent in pos_tag_sent_list:
+            for tuple_pair in sent:
+                word = tuple_pair[0]
+                pos_tag = tuple_pair[1]
+                #gives correct pos_tag
+                pos_tag = get_wordnet_pos(pos_tag)
+                #makes words lowercase
+                word = word.lower()
+                lem_ready_word_list.append((word, pos_tag))
         
-for sent in lemmatized_word_list:
-    for tuple_pair in lemmatized_word_list:
-        pass
-        #check pos_tag to match some WordCategories
-        #check for word in those WordCategories
-        #assign values
-'''        
+        #lemmatize
+        for tuple_pair in lem_ready_word_list:
+            word = tuple_pair[0]
+            pos_tag = tuple_pair[1]
+            #makes sure words with no equivalent wordnet pos_tag still get lemmatized
+            if pos_tag == '':
+                lem_word = wordnet_lemmatizer.lemmatize(word)
+                final_words.append(lem_word)
+            #general lemmatization
+            else:
+                lem_word = wordnet_lemmatizer.lemmatize(word, pos=pos_tag)
+                final_words.append(lem_word)
+    except UnicodeDecodeError:
+        story_error.append(fileName)
+        
+#print final_words
+print story_error
 
+#print len(sent_tokenize_list)
+#print sent_tokenize_list
+#print pos_tag_sent_list
+#print lem_ready_word_list
+#print final_words
+ 
 
 
 
