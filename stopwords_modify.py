e8736bf878665fd78a9145b2d10844a0f1af17dc
Madelyn Payne <mpayne@mit.edu>
2016-06-30 17:49:20 -0400

    Simplify to list comp

--- stopwords_modify.py
+++ stopwords_modify.py
@@ -4,13 +4,8 @@ Created on Wed Jun 29 15:26:43 2016
 
 @author: Madelyn
 """
-
-def modify_words2(final_words_modify1, custom_stopwords, stopwords_removed):
-    for word in final_words_modify1:
-        if word in custom_stopwords:
-            stopwords_removed.append(word)
-            final_words_modify1.remove(word)
-    final_words_modify2 = final_words_modify1
-    return (final_words_modify2, stopwords_removed)
-    
-        
+  
+def modify_words2(final_words_modify_set, custom_stopwords, removed_words):
+    final_words_modify2 = [x for x in final_words_modify_set if x not in custom_stopwords]
+    removed_words = [x for x in final_words_modify_set if x in custom_stopwords]
+    return (final_words_modify2, removed_words)
