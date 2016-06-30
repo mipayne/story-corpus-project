a75ca93327e803cfa6e83b69abc85f09d8c8915b
Madelyn Payne <mpayne@mit.edu>
2016-06-30 17:44:23 -0400

    Passes real words with punctuation

--- punctuation_modify.py
+++ punctuation_modify.py
@@ -4,35 +4,40 @@ Created on Wed Jun 29 15:47:42 2016
 
 @author: Madelyn
 """
+import re
+#final_words = ["'please", '.', 'it', "'s", "''", "``", "isn't"]
 
-#final_words = ["san", "francisco", "new", "york", "do", "n't", "we", "'ll", 'please', '.', 'it', "'s"]
-
-#for i in len(final_words):
-#    current_word = final_words[i]
-#    word_before = final_words[-i]
-#    if ((current_word[0].isalnum() == False:) and len(current_word) <= 4)
-#        contraction = word_before + current_word
-#        final_words_fixed.append(contraction)
-#        for 
-#        for j in len(final_words):
-#            while j < (i-1) continue:
-#            if final_words[j] == word_before:
-#                rep_count += 1
-#        final_words_fixed.remove(final_words[-i])
-#    elif i == "n't":
-#    else:
-#        final_words_fixed.append(final_words[i])
-
-#final_words_modify1
-#remove duplicates
-#apply custom_stopwords
-#remove punctuation (without removing words with dashes and apostrophes)
- #removed_words = []
+def modify_words3(final_words, removed_words):
+    contraction_list = ["what's", "it's", "i'm", "you've", "you'll",\
+        "i've", "he'd", "he's", "we'll", "you're", "i'll", "don't", \
+        "wouldn't", "wasn't", "isn't", "won't", "didn't", "doesn't",\
+        "can't", "couldn't", "let's", "don't", "wouldn't", "wasn't",\
+        "isn't", "won't", "didn't", "doesn't", "can't", "couldn't"]
     
-#    for word in final_words:
-#        if word.isalnum() == False: #and len(word) <= 2:#includes "'s", punctuation, 
-#            removed_words.append(word)
-#            final_words.remove(word)
+    final_words_modify3 = final_words[:]    
+    for word in final_words:
+        if (word.isalnum() == False):
+            if (len(word) > 3 and word not in contraction_list):
+                #print word
+                new_word = re.sub(r'[^\w]','', word)
+                #print new_word
+                final_words_modify3.append(new_word)
+                removed_words.append(word)
+                final_words_modify3.remove(word)
+            elif len(word) <= 3:#includes "'s", punctuation, "''", "``"
+                removed_words.append(word)
+                final_words_modify3.remove(word)
+        
+    return (final_words_modify3, removed_words)
+
+#print 'removed_words:'
+#print removed_words
+#print 'final_modify3'
+#print final_words_modify3
+
+    #final_words_modify1
+    #remove duplicates
+    #apply custom_stopwords
+    #remove punctuation (without removing words with dashes and apostrophes)
+     #removed_words = []
     
-#    print final_words
-#    print removed_words
