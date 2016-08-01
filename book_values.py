ca462c4368dc74c0e2e3632573a14acb47ab9ca8
Madelyn Payne <mpayne@mit.edu>
2016-08-01 16:19:04 -0400

    Create Book class/Look at avg word length

--- book_values.py
+++ book_values.py
@@ -27,6 +27,57 @@ allsheet_dict = modify_dictionary_from_excel.modify_dictionary(allsheet_dict)
 ezsheet_dict = modify_dictionary_from_excel.modify_dictionary(ezsheet_dict)
 custom_stopwords = modify_dictionary_from_excel.create_custom_stopword_list(allsheet_dict)
 
+class Book(object):
+    """A book in Children's StoryCorpus
+    
+    Attributes:
+        filename: string representing location of file
+        title: string representing the book's title
+        word_count_with_stopwords = len(final_words_modify3)
+        word_count_no_stopwords = len(final_words_modify4)
+        # in easy = final_words_modify4 - check_hard_list
+        # not in a list = rejected_words
+        # in all = check_hard_list - rejected_words
+        avg_length_final_words = final_words_modify4/len(final_words_modify4) 
+     """
+
+    def __init__(self, title, filename, word_count_with_stopwords,\
+        word_count_no_stopwords, num_ez_words, num_no_list_words, num_all_words,\
+        avg_length_final_words):
+        """Return a new Book object"""
+        self.filename = filename
+        self.title = title
+        self.word_count_with_stopwords = word_count_with_stopwords
+        self.word_count_no_stopwords = word_count_no_stopwords
+        self.num_ez_words = num_ez_words
+        self.num_no_list_words = num_no_list_words
+        self.num_all_words = num_all_words
+        self.avg_length_final_words = avg_length_final_words
+        
+    def get_filename(self):
+        return self.filename
+        
+    def get_title(self):
+        return self.title
+    
+    def get_word_count_with_stopwords(self):
+        return self.word_count_with_stopwords
+        
+    def get_word_count_no_stopwords(self):
+        return self.word_count_no_stopwords
+        
+    def get_num_ez_words(self):
+        return self.num_ez_words
+    
+    def get_num_no_list_words(self):
+        return self.num_no_list_words
+        
+    def get_num_all_words(self):
+        return self.num_all_words
+        
+    def get_avg_length_final_words(self):
+        return self.avg_length_final_words
+        
 def calculate_book_values(input_list, dictionary, word_point, tot_word_value, word_key_pair_dict):
     remain_list = []
     for word in input_list:
@@ -64,7 +115,7 @@ for book in book_words:
     final_words = book_words[book]
     temp_list = []
     for word in final_words:
-        print word[1]
+        #print word[1]
         if word[1] != "NNP":
             temp_list.append(word[0])
         else:
@@ -72,7 +123,9 @@ for book in book_words:
     book_words[book] = temp_list
             
 #print book_words
-print removed_propnouns            
+#print removed_propnouns            
+
+book_object_list = [] 
 
 for book in book_words:
     final_words = book_words[book]
@@ -97,9 +150,29 @@ for book in book_words:
         stopwords_modify.stopword_modify(final_words_modify3, custom_stopwords, removed_words)
     #print 'removed_words:'
 #    print removed_words
-#    print 'final_words_final'
-#    print final_words_modify4
+    #print 'final_words_final'
+    #print final_words_modify4
 
+    if book == './resources/converted/StoryCorpus/Tap_the_Jam.txt':
+        print book[34:]
+        print 'word_count_words'
+        print final_words_modify3
+        print 'final_words_final'
+        print final_words_modify4
+        
+    if book == './resources/converted/StoryCorpus/The_Ram_and_The_Jam.txt':
+        print book[34:]
+        print 'word_count_words'
+        print final_words_modify3
+        print 'final_words_final'
+        print final_words_modify4
+    
+    if book == './resources/converted/StoryCorpus/The_Dinosaur_Who_Lived_In_My_Backyard.txt':
+        print book[34:]
+        print 'word_count_words'
+        print final_words_modify3
+        print 'final_words_final'
+        print final_words_modify4
     rejected_words = [] 
     check_hard_list = []
     
@@ -134,23 +207,99 @@ for book in book_words:
     #print 'average book value:'
     #print avg_book_difficulty
     value_dict[book] = (tot_word_value, word_count_no_stopwords, avg_book_diff_no_stopwords, word_count_with_stopwords, avg_book_diff_with_stopwords)
+    
+    #for class change
+    filename = book
+    title = book[34:]
+    word_count_with_stopwords = float(len(final_words_modify3))
+    word_count_no_stopwords = float(len(final_words_modify4))
+    num_ez_words = word_count_no_stopwords - len(check_hard_list)
+    num_no_list_words = len(rejected_words)
+    num_all_words = len(check_hard_list) - num_no_list_words
+    len_count = 0
+    for word in final_words_modify4:
+        len_count += len(word)
+    avg_length_final_words = len_count/word_count_no_stopwords
+
+
+
+
+       
+
+
+    x = Book(book[34:], book, word_count_with_stopwords,\
+        word_count_no_stopwords, num_ez_words, num_no_list_words,\
+        num_all_words, avg_length_final_words)
+    
+    print x.get_filename()
+    
+    book_object_list.append(x)
+    
+
+for book in book_object_list:
+    book.get_title()
+    if book.get_title() == 'The_Ram_and_The_Jam.txt':
+        print "Wcount w/ Stopwords"
+        print book.get_word_count_with_stopwords()
+        print "Wcount"
+        print book.get_word_count_no_stopwords()
+        print "Easy words: ", book.get_num_ez_words()
+        print "All words: ", book.get_num_all_words()
+        print "No_list words: ", book.get_num_no_list_words()
+        print "Avg word length: ", book.get_avg_length_final_words ()
+        
+        
+
+
 
 
 #clean return values
 
 for book in value_dict:
     book_name = book[34:]
-    print book_name
-    tot_word_value, word_count, avg_book_diff, word_count_stop, avg_book_diff_stop = value_dict[book]
-    print "Total Value: ", tot_word_value
-    print "Total Words (no stopwords): ", word_count
-    print "Total Words (stopwords): ", word_count_stop
-    print "Difficulty (no stopwords): ", avg_book_diff
-    print "Difficulty (stopwords): ", avg_book_diff_stop
-    print "\n"
+#    print book_name
+#    tot_word_value, word_count, avg_book_diff, word_count_stop, avg_book_diff_stop = value_dict[book]
+#    print "Total Value: ", tot_word_value
+#    print "Total Words (no stopwords): ", word_count
+#    print "Total Words (stopwords): ", word_count_stop
+#    print "Difficulty (no stopwords): ", avg_book_diff
+#    print "Difficulty (stopwords): ", avg_book_diff_stop
+#    print "\n"
 #print value_dict
 #print len('./resources/converted/StoryCorpus/')
 
+ordered_list = []
+
+for book in value_dict:
+    book_name = book[34:]
+    tot_word_value, word_count, avg_book_diff, word_count_stop, avg_book_diff_stop = value_dict[book]
+    tuple_count = 0
+    first_book = False
+    if len(ordered_list) == 0:
+        #print "first_book"
+        first_book = True
+        ordered_list.append((book_name, avg_book_diff))
+    #print ordered_list
+    #print len(ordered_list)
+    for tuple_pair in ordered_list:
+        temp_list = []
+        if first_book == True:
+            break
+        tuple_count += 1
+        if avg_book_diff > tuple_pair[1]:
+            temp_list = ordered_list[:(tuple_count-1)]
+            temp_list.append((book_name, avg_book_diff))
+            temp_list.extend(ordered_list[(tuple_count-1):])
+            ordered_list = temp_list[:]
+            break
+        elif tuple_count == len(ordered_list):
+            ordered_list.append((book_name, avg_book_diff))
+            #print ordered_list
+            break
+        #else:
+            #print 'NO!'
+            
+#print ordered_list           
 '''
 NEXT STEP:
 consider average difficulty of each group ('Nouns', 'Adjectives')
